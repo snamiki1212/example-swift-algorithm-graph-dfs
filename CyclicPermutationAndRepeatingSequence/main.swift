bf53a8aae6e3b6ff5b75f13aee26fa1fd52165dd
snamiki1212 <snamiki1212@gmail.com>
2021-08-11 14:00:46 -0700

    Add cyclic permutation logic

--- CyclicPermutationAndRepeatingSequence/main.swift
+++ CyclicPermutationAndRepeatingSequence/main.swift
@@ -9,25 +9,45 @@ import Foundation
 
 func main(){
     let numOfCases = Int(readLine()!)!
-    print("\n", "numOfCases", numOfCases)
+    
     for _ in 0...numOfCases {
         let len = Int(readLine()!)!
-        print("len", len);
+        
         let list = readLine()!.split(separator: " ").map { str in
             Int(str)!
         }
-        var adjList = [[Int]](repeating: [], count: len + 1)
+        var adjList = [Int](repeating: -1, count: len + 1)
         for (from, to) in list.enumerated() {
-            adjList[from + 1].append(to)
+            adjList[from + 1] = to
         }
         printNumOfCycles(adjList: adjList)
     }
 }
 
-func printNumOfCycles(adjList: [[Int]]){
+func printNumOfCycles(adjList: [Int]){
     // count up how many cycles
     
-    print("adjList", adjList)
+    var visited = [Bool](repeating: false, count: adjList.count + 1)
+    var count = 0;
+    for from in 1..<adjList.count {
+        if visited[from] { continue }
+        var cycle = [Bool](repeating: false, count: adjList.count + 1)
+        if dfs(adjList, &visited, &cycle, from) { count += 1 }
+    }
+    
+    print("adjList", adjList, count)
+}
+
+func dfs(_ adjList: [Int], _ visited: inout [Bool], _ cycle: inout [Bool], _ from: Int) -> Bool {
+    // base case
+    if visited[from] && cycle[from] { return true }
+    if visited[from] && !cycle[from] { return false }
+
+    // recursive case
+    visited[from] = true
+    cycle[from] = true
+    let to = adjList[from]
+    return dfs(adjList, &visited, &cycle, to)
 }
 
 main()
